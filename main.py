0bab873520559d8a3e1af8ea3a8218eb6d9950b3
Hans Maerki <hmaerki@github.com>
2020-04-26 21:25:26 +0200

    Added vscode workspace

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from flask import Flask 
-from flask_socketio import SocketIO, send
+import flask
+import flask_socketio
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
+socketio = flask_socketio.SocketIO(app)
 
 @socketio.on('message')
 def handleMessage(msg):
 	print('Message: ' + msg)
-	send(msg, broadcast=True)
+	socketio.send(msg, broadcast=True)
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
 
 if __name__ == '__main__':
 	socketio.run(app)
