0ebcfec0a5e91adbb758bf02ce548076b2b8a868
Hans Märki <hans.maerki@zhinst.com>
2020-04-30 19:56:27 +0200

    Works on windos using svg.js

--- main.py
+++ main.py
@@ -10,28 +10,26 @@ socketio = flask_socketio.SocketIO(app)
 
 @socketio.on('message')
 def handleMessage(msg):
-	print(f'Message: {msg}')
-	socketio.send(f'MESSAGE:{msg}', broadcast=True)
+  print(f'Message: {msg}')
+  socketio.send(f'MESSAGE:{msg}', broadcast=True)
 
 @app.route('/')
 def index():
     return flask.render_template('index.html')
 
 class Timer(threading.Thread):
-	def __init__(self, socketio):
-		threading.Thread.__init__(self)
-		self.socketio = socketio
+  def __init__(self, socketio):
+    threading.Thread.__init__(self)
+    self.socketio = socketio
 
-	def run(self):
-		while True:
-			time.sleep(1.0)
-			msg = time.strftime('%H:%M:%S')
-			socketio.send(f'TIME:{msg}')
+  def run(self):
+    while True:
+      time.sleep(1.0)
+      msg = time.strftime('%H:%M:%S')
+      socketio.send(f'TIME:{msg}')
 
 if __name__ == '__main__':
+  timer = Timer(socketio)
+  timer.start()
 
-
-	timer = Timer(socketio)
-	timer.start()
-
-	socketio.run(app)
+  socketio.run(app)
