fb88daab65ea72980f672c27ebab7d6651e741ad
Hans Märki <hans.maerki@zhinst.com>
2020-05-19 16:25:52 +0200

    New directory structure.

--- image_preparation/httpserver.bat
+++
@@ -1,6 +0,0 @@
-@rem python -m http.server
-@rem usage: server.py [-h] [--cgi] [--bind ADDRESS] [--directory DIRECTORY] [port]
-
-python -m http.server --bind 0.0.0.0 --directory .. 5000
-@rem python -m http.server --bind 127.0.0.1 --directory .. 5000
-pass
