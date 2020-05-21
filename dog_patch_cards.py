6f81721b52f919d8684a9e9d9def462aa15558e4
Hans Märki <hans.maerki@zhinst.com>
2020-05-21 13:33:41 +0200

    CardsPatcher using sax

--- dog_patch_cards.py
+++ dog_patch_cards.py
@@ -1,5 +1,7 @@
 import pathlib
-import xml.etree.ElementTree
+import xml.sax
+import xml.sax.xmlreader
+import xml.sax.saxutils
 import dog_constants
 
 # A dog game contains 2 cards of black spade, but 6 cards of joker
@@ -8,21 +10,27 @@ LIST_NUMBER_CARDS = ('2', '6')
 DIRECTORY_OF_THIS_FILE = pathlib.Path(__file__).parent
 DIRECTORY_CARDS_ORI = DIRECTORY_OF_THIS_FILE / 'static' / 'boardori' / 'cards'
 
-class CardsPatcher:
+class CardsPatcher(xml.sax.saxutils.XMLFilterBase):
     def __init__(self, dbc: dog_constants.DogBoardConstants):
+        parent = xml.sax.make_parser()
+        super().__init__(parent)
         self.__dbc = dbc
 
-    def __convert_svg(self, tree):
-        card_width = self.__dbc.CARD_SIZE.real
-        card_height = self.__dbc.CARD_SIZE.imag
-        xml_declaration = tree.find(".")
-        xml_declaration.attrib['height'] = str(card_height)
-        xml_declaration.attrib['width'] = str(card_width)
-        xml_declaration.attrib['x'] = str(card_width/2)
-        xml_declaration.attrib['y'] = str(card_height/2)
+    def startElement(self, name, attrs):
+        if name == 'svg':
+            card_width = self.__dbc.CARD_SIZE.real
+            card_height = self.__dbc.CARD_SIZE.imag
+            attrs = {key:value for key, value in attrs.items()}
+            attrs['height'] = str(card_height)
+            attrs['width'] = str(card_width)
+            attrs['x'] = str(card_width/2)
+            attrs['y'] = str(card_height/2)
 
-    def __convert_cards_for_board(self):
-        directory_cards_new = DIRECTORY_OF_THIS_FILE / self.__dbc.BOARD_DIRECTORY_RELATIVE / 'cards'
+        super().startElement(name, attrs)
+
+    @classmethod
+    def __convert_cards_for_board(cls, dbc: dog_constants.DogBoardConstants):
+        directory_cards_new = DIRECTORY_OF_THIS_FILE / dbc.BOARD_DIRECTORY_RELATIVE / 'cards'
         for number_cards in LIST_NUMBER_CARDS:
             directory_new = directory_cards_new.joinpath(number_cards)
             if not directory_new.exists():
@@ -30,18 +38,18 @@ class CardsPatcher:
 
             for filenameOri in DIRECTORY_CARDS_ORI.joinpath(number_cards).glob('*.svg'):
                 filenameNew = directory_new.joinpath(filenameOri.name)
-                print(f'*** {filenameOri} -> {filenameNew}')
+                # print(f'*** {filenameOri} -> {filenameNew}')
 
-                tree = xml.etree.ElementTree.parse(filenameOri)
-                self.__convert_svg(tree)
-                xml.etree.ElementTree.register_namespace('', 'http://www.w3.org/2000/svg')
-                tree.write(filenameNew, encoding='utf-8', xml_declaration=True)
+                reader = CardsPatcher(dbc)
+                with open(filenameNew, 'w', encoding='utf-8') as f:
+                    handler = xml.sax.saxutils.XMLGenerator(f, encoding='utf-8')
+                    reader.setContentHandler(handler)
+                    reader.parse(str(filenameOri))
 
     @classmethod
     def convert_cards(cls):
         for dbc in dog_constants.LIST_DOG_BOARD_CONSTANTS:
-            cp = CardsPatcher(dbc)
-            cp.__convert_cards_for_board()
+            cls.__convert_cards_for_board(dbc)
 
 if __name__ == '__main__':
     CardsPatcher.convert_cards()
